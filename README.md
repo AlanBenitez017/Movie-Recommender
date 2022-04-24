cecd51c423ed122e4b8285eafba5d48bb9b6c855
Alan <alanbenitez017@gmail.com>
2022-04-23 20:43:31 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project2ArtInt
 Project 2 of Artificial Intelligence about movie recommender
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install sklearn
--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Movie Recommendation System
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,57 +6,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 from ast import literal_eval
+import io
+import csv
 
 
-# In[2]:
+  
+creditsDF = pd.read_csv('tmdb_5000_credits.csv', index_col=0)
+moviesDF = pd.read_csv('tmdb_5000_movies.csv', index_col=0)
 
 
-path = "./Desktop/TechVidvan/movie_recommendation"
-credits_df = pd.read_csv(path + "/tmdb_credits.csv")
-movies_df = pd.read_csv(path + "/tmdb_movies.csv")
+print(moviesDF.head())
+print(creditsDF.head())
+
+moviesDF = pd.merge(creditsDF, moviesDF, on = "id", how = "inner")
 
 
-# In[3]:
+print(moviesDF.head())
 
-
-movies_df.head()
-
-
-# In[4]:
-
-
-credits_df.head()
-
-
-# In[5]:
-
-
-credits_df.columns = ['id','tittle','cast','crew']
-movies_df = movies_df.merge(credits_df, on="id")
-
-
-# In[6]:
-
-
-movies_df.head()
-
-
-# In[7]:
-
-
-# Demographic Filtering
-C = movies_df["vote_average"].mean()
-m = movies_df["vote_count"].quantile(0.9)
-
+C = moviesDF["vote_average"].mean()
+m = moviesDF["vote_count"].quantile(0.9)
 print("C: ", C)
 print("m: ", m)
 
-new_movies_df = movies_df.copy().loc[movies_df["vote_count"] >= m]
-print(new_movies_df.shape)
-
-
-# In[8]:
-
+newMoviesDF = moviesDF.copy().loc[moviesDF["vote_count"] >= m]
+print(newMoviesDF.shape)
 
 def weighted_rating(x, C=C, m=m):
     v = x["vote_count"]
@@ -72,63 +37,35 @@ def weighted_rating(x, C=C, m=m):
 
     return (v/(v + m) * R) + (m/(v + m) * C)
 
+newMoviesDF["score"] = newMoviesDF.apply(weighted_rating, axis=1)
+newMoviesDF = newMoviesDF.sort_values('score', ascending=False)
 
-# In[9]:
+newMoviesDF[["title_y", "vote_count", "vote_average", "score"]].head(5)
 
-
-new_movies_df["score"] = new_movies_df.apply(weighted_rating, axis=1)
-new_movies_df = new_movies_df.sort_values('score', ascending=False)
-
-new_movies_df[["title", "vote_count", "vote_average", "score"]].head(10)
-
-
-# In[10]:
-
-
-# Plot top 10 movies
 def plot():
-    popularity = movies_df.sort_values("popularity", ascending=False)
+    popularity = moviesDF.sort_values("popularity", ascending=False)
     plt.figure(figsize=(12, 6))
-    plt.barh(popularity["title"].head(10), popularity["popularity"].head(10), align="center", color="skyblue")
+    plt.barh(popularity["title_y"].head(10), popularity["popularity"].head(10), align="center", color="skyblue")
     plt.gca().invert_yaxis()
     plt.title("Top 10 movies")
     plt.xlabel("Popularity")
     plt.show()
-    
 
 plot()
 
-
-# In[11]:
-
-
-# Content based Filtering
-print(movies_df["overview"].head(5))
-
-
-# In[12]:
-
+print(moviesDF["overview"].head(5))
 
 tfidf = TfidfVectorizer(stop_words="english")
-movies_df["overview"] = movies_df["overview"].fillna("")
+moviesDF["overview"] = moviesDF["overview"].fillna("")
 
-tfidf_matrix = tfidf.fit_transform(movies_df["overview"])
+tfidf_matrix = tfidf.fit_transform(moviesDF["overview"])
 print(tfidf_matrix.shape)
 
-
-# In[13]:
-
-
-# Compute similarity
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 print(cosine_sim.shape)
 
-indices = pd.Series(movies_df.index, index=movies_df["title"]).drop_duplicates()
+indices = pd.Series(moviesDF.index, index=moviesDF["title_y"]).drop_duplicates()
 print(indices.head())
-
-
-# In[14]:
-
 
 def get_recommendations(title, cosine_sim=cosine_sim):
     """
@@ -147,134 +84,13 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     # (a, b) where a is id of movie, b is sim_score
 
     movies_indices = [ind[0] for ind in sim_scores]
-    movies = movies_df["title"].iloc[movies_indices]
+    movies = moviesDF["title_y"].iloc[movies_indices]
     return movies
 
-
-# In[15]:
-
-
-print("################ Content Based Filtering - plot#############")
-print()
-print("Recommendations for The Dark Knight Rises")
-print(get_recommendations("The Dark Knight Rises"))
-print()
-print("Recommendations for Avengers")
-print(get_recommendations("The Avengers"))
-
-
-# In[16]:
-
-
-features = ["cast", "crew", "keywords", "genres"]
-
-for feature in features:
-    movies_df[feature] = movies_df[feature].apply(literal_eval)
-
-movies_df[features].head(10)
-
-
-# In[17]:
-
-
-def get_director(x):
-    for i in x:
-        if i["job"] == "Director":
-            return i["name"]
-    return np.nan
-
-
-# In[18]:
-
-
-def get_list(x):
-    if isinstance(x, list):
-        names = [i["name"] for i in x]
-
-        if len(names) > 3:
-            names = names[:3]
-
-        return names
-
-    return []
-
-
-# In[19]:
-
-
-movies_df["director"] = movies_df["crew"].apply(get_director)
-
-features = ["cast", "keywords", "genres"]
-for feature in features:
-    movies_df[feature] = movies_df[feature].apply(get_list)
-
-
-# In[21]:
-
-
-movies_df[['title', 'cast', 'director', 'keywords', 'genres']].head()
-
-
-# In[22]:
-
-
-def clean_data(x):
-    if isinstance(x, list):
-        return [str.lower(i.replace(" ", "")) for i in x]
-    else:
-        if isinstance(x, str):
-            return str.lower(x.replace(" ", ""))
-        else:
-            return ""
-
-
-# In[23]:
-
-
-features = ['cast', 'keywords', 'director', 'genres']
-for feature in features:
-    movies_df[feature] = movies_df[feature].apply(clean_data)
-
-
-# In[24]:
-
-
-def create_soup(x):
-    return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + x['director'] + ' ' + ' '.join(x['genres'])
-
-
-movies_df["soup"] = movies_df.apply(create_soup, axis=1)
-print(movies_df["soup"].head())
-
-
-# In[25]:
-
-
-count_vectorizer = CountVectorizer(stop_words="english")
-count_matrix = count_vectorizer.fit_transform(movies_df["soup"])
-
-print(count_matrix.shape)
-
-cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
-print(cosine_sim2.shape)
-
-movies_df = movies_df.reset_index()
-indices = pd.Series(movies_df.index, index=movies_df['title'])
-
-
-# In[26]:
-
-
-print("################ Content Based System - metadata #############")
-print("Recommendations for The Dark Knight Rises")
-print(get_recommendations("The Dark Knight Rises", cosine_sim2))
-print()
-print("Recommendations for Avengers")
-print(get_recommendations("The Avengers", cosine_sim2))
-
-
-# In[ ]:
-
+print("Content based filtering")
+val = input("Enter the name of the movie that you would like to see the Recomendations: ")
+print("Recommendations for " + val)
+print(get_recommendations(val))
 
 
 
