be5e1bcc2c6d55636ed2be00ac62b6d33cb3fd15
Alan Benitez <alanbenitez017@gmail.com>
2022-04-24 19:48:58 -0500

    Update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Project2ArtInt
-Project 2 of Artificial Intelligence about movie recommender
-pip install numpy
-pip install pandas
-pip install matplotlib
-pip install sklearn
+Project 2 of Artificial Intelligence about movie recommender using Content-based filtering<br />
+Please run the following commands to install the libraries that will be used in order to run the program<br />
+pip install numpy<br />
+pip install pandas<br />
+pip install matplotlib<br />
+pip install sklearn<br />
+<br />
+After installing these libraries, please run the run.bat file in the command prompt in order to run the program. You can do so by simply running **run**
--- main.py
+++ main.py
@@ -6,91 +6,134 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 from ast import literal_eval
-import io
-import csv
+import sys
+
+class MovieRecommender:
+
+    # Overloaded constructor 
+    def __init__(self, filename1, filename2, creditsDF, moviesDF):
+        self.filename1 = filename1
+        self.filename2 = filename2
+        self.creditsDF = creditsDF
+        self.moviesDF = moviesDF
+        #self.readDatasets(filename1, filename2)
+
+    # Method to read the datasets
+    def readDatasets(self, filename1, filename2):
+        self.creditsDF = pd.read_csv(filename1, index_col=0)
+        self.moviesDF = pd.read_csv(filename2, index_col=0)
+        print(self.moviesDF.head())
+        print(self.creditsDF.head())
+        self.moviesDF = pd.merge(self.creditsDF, self.moviesDF, on = "id", how = "inner")
+        #return moviesDF
 
 
-  
-creditsDF = pd.read_csv('tmdb_5000_credits.csv', index_col=0)
-moviesDF = pd.read_csv('tmdb_5000_movies.csv', index_col=0)
+    def calculateAverageAndQuantiles(self, moviesDF):
+        averageVote = moviesDF["vote_average"].mean()
+        quantile90percent = moviesDF["vote_count"].quantile(0.9)
+        print("Average Vote: ", averageVote)
+        print("Quantile 90%: ", quantile90percent)
+
+        moviesDF = moviesDF.copy().loc[moviesDF["vote_count"] >= quantile90percent]
+        print(moviesDF.shape)
+        #print(self.weighted_rating(newMoviesDF, averageVote, quantile90percent))
+        #self.weighted_rating(newMoviesDF, averageVote, quantile90percent)
+        self.updateAndPrint(moviesDF, averageVote, quantile90percent)
+        
+    
+
+    def weighted_rating(self, x, averageVote, quantile90percent):
+        voteCount = x["vote_count"]
+        R = x["vote_average"]
+        #print("R = " + R)
+        return (voteCount/(voteCount + quantile90percent) * R) + (quantile90percent/(voteCount + quantile90percent) * averageVote)
+
+    def updateAndPrint(self, moviesDF, averageVote, quantile90percent):
+        try:
+            moviesDF["score"] = self.moviesDF.apply(self.weighted_rating(moviesDF, averageVote, quantile90percent), axis=1)   #applying the weighted_rating for each row
+            moviesDF = moviesDF.sort_values('score', ascending=False)
+        except AssertionError as msg:
+            print(msg)
+        moviesDF.head(5)
+
+    # Plot a graph of the top 10 movies based on popularity of the movies
+    def plot(self, moviesDF):
+        popularity = moviesDF.sort_values("popularity", ascending=False)
+        plt.figure(figsize=(12, 6))
+        plt.barh(popularity["title_y"].head(10), popularity["popularity"].head(10), align="center", color="skyblue")
+        plt.gca().invert_yaxis()
+        plt.title("Top 10 movies")
+        plt.xlabel("Popularity")
+        plt.show()
 
 
-print(moviesDF.head())
-print(creditsDF.head())
+    def cosineSimilarity(self, moviesDF):
+        print(moviesDF["overview"].head(5))
 
-moviesDF = pd.merge(creditsDF, moviesDF, on = "id", how = "inner")
+        tfidf = TfidfVectorizer(stop_words="english")
+        moviesDF["overview"] = moviesDF["overview"].fillna("")
+
+        tfidf_matrix = tfidf.fit_transform(moviesDF["overview"])
+        print(tfidf_matrix.shape)
+
+        cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+        print(cosine_sim.shape)
+
+        return cosine_sim
+
+    def getIndices(self, moviesDF):
+        index = moviesDF["title_y"]
+        indices = pd.Series(moviesDF.index, index).drop_duplicates()
+        print(indices.head())
+        return indices
+        #indices = pd.Series(moviesDF.index, index=moviesDF["title_y"]).drop_duplicates()
+        #print(indices.head())
+        #return indices
+
+    def getRecommendations(self, title, cosine_sim, indices):
+        """
+        in this function,
+            we take the cosine score of given movie
+            sort them based on cosine score (movie_id, cosine_score)
+            take the next 10 values because the first entry is itself
+            get those movie indices
+            map those indices to titles
+            return title list
+        """
+
+        idx = indices[title]
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:11]
+        # (a, b) where a is id of movie, b is sim_score
+
+        movies_indices = [ind[0] for ind in sim_scores]
+        movies = self.moviesDF["title_y"].iloc[movies_indices]
+        print(movies)
+        return movies
+
+    #Runnig the program
+    def runProgram(self, cos_sim, indices):
+        print("##########    CONTENT-BASED FILTERING    ###########")
+        title = input("Enter the name of the movie that you would like to see the Recomendations: ")
+        print("Recommendations for " + title)
+        self.getRecommendations(title, cos_sim, indices)
+
+if __name__ == "__main__":
+    creditsFileName = sys.argv[1]
+    moviesFileName = sys.argv[2]
+    creditsDF = ""
+    moviesDF = ""
+    myMovieRecommender = MovieRecommender(creditsFileName, moviesFileName, creditsDF, moviesDF)
+    myMovieRecommender.readDatasets(creditsFileName, moviesFileName)
+    myMovieRecommender.calculateAverageAndQuantiles(myMovieRecommender.moviesDF)
+    myMovieRecommender.plot(myMovieRecommender.moviesDF)
+    cos_sim = myMovieRecommender.cosineSimilarity(myMovieRecommender.moviesDF)
+    indices = myMovieRecommender.getIndices(myMovieRecommender.moviesDF)
+    myMovieRecommender.runProgram(cos_sim, indices)
 
 
-print(moviesDF.head())
 
-C = moviesDF["vote_average"].mean()
-m = moviesDF["vote_count"].quantile(0.9)
-print("C: ", C)
-print("m: ", m)
-
-newMoviesDF = moviesDF.copy().loc[moviesDF["vote_count"] >= m]
-print(newMoviesDF.shape)
-
-def weighted_rating(x, C=C, m=m):
-    v = x["vote_count"]
-    R = x["vote_average"]
-
-    return (v/(v + m) * R) + (m/(v + m) * C)
-
-newMoviesDF["score"] = newMoviesDF.apply(weighted_rating, axis=1)
-newMoviesDF = newMoviesDF.sort_values('score', ascending=False)
-
-newMoviesDF[["title_y", "vote_count", "vote_average", "score"]].head(5)
-
-def plot():
-    popularity = moviesDF.sort_values("popularity", ascending=False)
-    plt.figure(figsize=(12, 6))
-    plt.barh(popularity["title_y"].head(10), popularity["popularity"].head(10), align="center", color="skyblue")
-    plt.gca().invert_yaxis()
-    plt.title("Top 10 movies")
-    plt.xlabel("Popularity")
-    plt.show()
-
-plot()
-
-print(moviesDF["overview"].head(5))
-
-tfidf = TfidfVectorizer(stop_words="english")
-moviesDF["overview"] = moviesDF["overview"].fillna("")
-
-tfidf_matrix = tfidf.fit_transform(moviesDF["overview"])
-print(tfidf_matrix.shape)
-
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-print(cosine_sim.shape)
-
-indices = pd.Series(moviesDF.index, index=moviesDF["title_y"]).drop_duplicates()
-print(indices.head())
-
-def get_recommendations(title, cosine_sim=cosine_sim):
-    """
-    in this function,
-        we take the cosine score of given movie
-        sort them based on cosine score (movie_id, cosine_score)
-        take the next 10 values because the first entry is itself
-        get those movie indices
-        map those indices to titles
-        return title list
-    """
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    # (a, b) where a is id of movie, b is sim_score
-
-    movies_indices = [ind[0] for ind in sim_scores]
-    movies = moviesDF["title_y"].iloc[movies_indices]
-    return movies
-
-print("Content based filtering")
-val = input("Enter the name of the movie that you would like to see the Recomendations: ")
-print("Recommendations for " + val)
-print(get_recommendations(val))
 
 
 
