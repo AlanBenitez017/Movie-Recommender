6f390920a206737fb06e782a89711060d3da44f7
Alan Benitez <alanbenitez017@gmail.com>
2022-04-24 20:21:48 -0500

    Update

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Student Names: Alan Benitez and Justin Gilmer
+# File Name: main.py
+# Project 2 - COP 4601
+# 24 April 2022
+# Movie Recommender using content-based filtering. The program attempts to guess what a user may like based on a title chosen by the user
+# ---------------------------------------------------------------------------
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,7 +24,6 @@ class MovieRecommender:
         self.filename2 = filename2
         self.creditsDF = creditsDF
         self.moviesDF = moviesDF
-        #self.readDatasets(filename1, filename2)
 
     # Method to read the datasets
     def readDatasets(self, filename1, filename2):
@@ -25,9 +32,8 @@ class MovieRecommender:
         print(self.moviesDF.head())
         print(self.creditsDF.head())
         self.moviesDF = pd.merge(self.creditsDF, self.moviesDF, on = "id", how = "inner")
-        #return moviesDF
 
-
+    # Method to calculate the average and the quantiles
     def calculateAverageAndQuantiles(self, moviesDF):
         averageVote = moviesDF["vote_average"].mean()
         quantile90percent = moviesDF["vote_count"].quantile(0.9)
@@ -36,18 +42,16 @@ class MovieRecommender:
 
         moviesDF = moviesDF.copy().loc[moviesDF["vote_count"] >= quantile90percent]
         print(moviesDF.shape)
-        #print(self.weighted_rating(newMoviesDF, averageVote, quantile90percent))
-        #self.weighted_rating(newMoviesDF, averageVote, quantile90percent)
         self.updateAndPrint(moviesDF, averageVote, quantile90percent)
         
-    
-
-    def weighted_rating(self, x, averageVote, quantile90percent):
-        voteCount = x["vote_count"]
-        R = x["vote_average"]
+    # Helper method to weight the rating of every movie
+    def weighted_rating(self, df, averageVote, quantile90percent):
+        voteCount = df["vote_count"]
+        R = df["vote_average"]
         #print("R = " + R)
         return (voteCount/(voteCount + quantile90percent) * R) + (quantile90percent/(voteCount + quantile90percent) * averageVote)
-
+    
+    # Adds a new column named score and populates it with the weighted rating of every row, then sorts it by scores and prints the first 5
     def updateAndPrint(self, moviesDF, averageVote, quantile90percent):
         try:
             moviesDF["score"] = self.moviesDF.apply(self.weighted_rating(moviesDF, averageVote, quantile90percent), axis=1)   #applying the weighted_rating for each row
@@ -66,7 +70,7 @@ class MovieRecommender:
         plt.xlabel("Popularity")
         plt.show()
 
-
+    # Does the cosine similarity using the overview column of the dataset
     def cosineSimilarity(self, moviesDF):
         print(moviesDF["overview"].head(5))
 
@@ -81,44 +85,36 @@ class MovieRecommender:
 
         return cosine_sim
 
+    # Creates indices given all the titles of the dataset, adn then returns those indices 
     def getIndices(self, moviesDF):
         index = moviesDF["title_y"]
         indices = pd.Series(moviesDF.index, index).drop_duplicates()
         print(indices.head())
         return indices
-        #indices = pd.Series(moviesDF.index, index=moviesDF["title_y"]).drop_duplicates()
-        #print(indices.head())
-        #return indices
 
+    # Takes the cosine score of a specific movie and sort them by cosine score. 
+    # Select the following 10 values because the first entry is itself. 
+    # Retrieves those movie indices, maps it to titles, and return the movie list.
     def getRecommendations(self, title, cosine_sim, indices):
-        """
-        in this function,
-            we take the cosine score of given movie
-            sort them based on cosine score (movie_id, cosine_score)
-            take the next 10 values because the first entry is itself
-            get those movie indices
-            map those indices to titles
-            return title list
-        """
 
         idx = indices[title]
         sim_scores = list(enumerate(cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         sim_scores = sim_scores[1:11]
-        # (a, b) where a is id of movie, b is sim_score
 
         movies_indices = [ind[0] for ind in sim_scores]
         movies = self.moviesDF["title_y"].iloc[movies_indices]
         print(movies)
         return movies
 
-    #Runnig the program
+    #Running the program
     def runProgram(self, cos_sim, indices):
         print("##########    CONTENT-BASED FILTERING    ###########")
         title = input("Enter the name of the movie that you would like to see the Recomendations: ")
         print("Recommendations for " + title)
         self.getRecommendations(title, cos_sim, indices)
 
+# Main
 if __name__ == "__main__":
     creditsFileName = sys.argv[1]
     moviesFileName = sys.argv[2]
